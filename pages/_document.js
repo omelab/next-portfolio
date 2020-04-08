64aa1d62052bc50d6c33aa55f348e227b5701a49
Abu Bakar <abu.bakar@apsissolutions.com>
2020-04-08 20:48:28 +0600

    isomorphic-unfetch install and get data form github api and display on page

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class myDocument extends Document{
                     <meta name="viewport" content="width=device-width"/>
                     <link
                         rel="stylesheet"
-                        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+                        href="/static/nprogress.min.css"
                     ></link>
                     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet"/>
                 </Head>
--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,56 @@
 
 import Link from 'next/link';
+
 import Layout from "../components/Layout";
 
-const About = () =>(
-    <Layout>
-        <h1>About</h1>
-        <Link  href="/">
-            <a> Go to Home</a>
-        </Link>
-        <p>A javascript Programmer</p>
-        <img src="/static/bg.jpg" alt=""/>
-    </Layout>
-);
-export default About;
+import fetch from 'isomorphic-unfetch';
+
+import { Component } from "react";
+
+export default class About extends Component{
+
+    static async getInitialProps(){
+
+        const res = await fetch('https://api.github.com/users/omelab');
+        const data = await res.json();
+
+        return { user:data }
+    }
+
+    //Client Side Rendering
+    /* 
+    //initial data
+    state = {
+        user : null
+    }
+
+    //set data when page load
+    componentDidMount(){
+        fetch('https://api.github.com/users/omelab')
+        .then(res => res.json())
+        .then(data =>{
+
+            this.setState({
+                user:data
+            });
+
+            console.log(data);
+        })
+    }*/
+
+
+    render(){
+        const { user } = this.props;
+        return(
+        <Layout>
+            {/* {JSON.stringify(this.state.user)} */}
+            <h1>About</h1>
+           
+            <p>{user.name}</p>
+            <img src={user.avatar_url} alt={user.name}/>
+        </Layout>
+        )
+    }
+    
+}
+ 
